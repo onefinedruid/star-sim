00ebecc8026f840507392429d705acb208aa0af5
Ian Weber <onefinedruid@yahoo.com>
2021-12-25 23:04:04 -0500

    // desc

--- gameloop.js
+++ gameloop.js
@@ -1,1 +1,10 @@
 document.getElementById("demo").innerHTML = "gameloop.js";
+
+// choose starting group
+// shuffle deck
+// each player draws 4 cards, keeping specials
+// turn order
+   // draw
+   // income
+   // 2 actions
+// win if secret goal met
