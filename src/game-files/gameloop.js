8ebbf2850b1df6ad04f9f0e1d8507eb6ac7a8869
Ian Weber <onefinedruid@yahoo.com>
2021-12-29 22:13:18 -0500

    add #terminal

--- src/game-files/gameloop.js
+++ src/game-files/gameloop.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(Draggable);
 
-Draggable.create("#card");
+Draggable.create("#card", {
+});
 
+// new game?
+document.getElementById('terminal-out').innerHTML = ">Start new game? Y/N";
+document.getElementById('terminal').value = "";
 // choose starting group
 // shuffle deck
 // each player draws 4 cards, keeping specials
