0182aa2e243b47c17d2fca97b8aec05b39879232
Ian Weber <onefinedruid@gmail.com>
2022-02-03 13:44:51 -0500

    fix card origin pos + implement playerbox containment + ul new ss

--- src/index.js
+++ src/index.js
@@ -19,31 +19,47 @@ import { gameloop } from './game-files/gameloop';
 
     // enable drag + grid
     function enableGrid() {
-    $("#card").draggable({ 
-        grid: [ 64,64 ],
-        snapTolerance: 64,
-        containment: "#canvas",
-        refreshPositions: false,
-    });
-
-    $('#card').position({
-        my: "center bottom",
-        within: "#playerbox1"
-    });
-
-    $("#card2").draggable({ 
-        grid: [ 64,64 ],
-        snapTolerance: 64,
-        containment: "#canvas",
-        refreshPositions: false,
-    });
-    $("#card3").draggable({ 
-        grid: [ 64,64 ],
-        snapTolerance: 64,
-        containment: "#canvas",
-        refreshPositions: false,
-    });
-    }
+        $('#card,#card2,#card3,#card4,#card5,#card6').each(function(){
+            $(this).draggable({ 
+                grid: [ 64,64 ],
+                snapTolerance: 64,
+                containment: "parent",
+                refreshPositions: false,
+                
+            })
+            $('#card').position({
+                my: "center bottom-2",
+                at: "center bottom-2",
+                of: "#playerbox1"
+            })
+            $('#card2').position({
+                my: "center bottom-2",
+                at: "center-95 bottom-2",
+                of: "#playerbox1"
+            })
+            $('#card3').position({
+                my: "center bottom-2",
+                at: "center+128 bottom-2",
+                of: "#playerbox1"
+            })
+            $('#card4').position({
+                my: "center top+1",
+                at: "center top+1",
+                of: "#playerbox2"
+            })
+            $('#card5').position({
+                my: "center top+1",
+                at: "center-96 top+1",
+                of: "#playerbox2"
+            })
+            $('#card6').position({
+                my: "center top+1",
+                at: "center+32 top+130",
+                of: "#playerbox2"
+            })
+        }
+    )};
+    
 
     enableTerm();
     enableGrid();
@@ -54,5 +70,5 @@ import { gameloop } from './game-files/gameloop';
 
     console.log(GameObjects.Pirates.Crew[0].name);
     gameloop();
-    terminalOut.innerHTML = GameObjects.Theologians.Crew[0].name;
+    terminalOut.innerHTML = GameObjects.Theologians.Crew[0].name + ", " + GameObjects.Theologians.Crew[0].artifacts + "/" + GameObjects.Theologians.Crew[0].goal + " Artifacts";
 
